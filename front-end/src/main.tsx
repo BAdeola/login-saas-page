81046ae426a1506b83362ceaaecae080b520791a
Brayan Adeolá <brayanadeola@gmail.com>
2026-03-09 13:18:58 -0300

    debug: "fixing the problem with the mobile cache, now the app always aks the new html"

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,7 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+
+window.addEventListener('vite:preloadError', (event) => {
+  event.preventDefault();
+  window.location.reload();
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
